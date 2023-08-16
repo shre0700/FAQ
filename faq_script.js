e531dd50649c2c36a74d8e7e70baaadbbeb81ba4
shre0700 <138860718+shre0700@users.noreply.github.com>
2023-08-16 19:40:02 +0530

    Add files via upload

---
+++ faq_script.js
@@ -0,0 +1,15 @@
+function show_ans1(){
+document.getElementById("q1").innerHTML = "You can invite upto 2 additional users in the Free Plan. There is no limit on team members for the Premium Plan.";
+}
+function show_ans2(){
+    document.getElementById("q2").innerHTML = "The maximum file upload size is upto 10GB.";
+}
+function show_ans3(){
+    document.getElementById("q3").innerHTML = "Click 'Forget Password' from the login page or 'Change Password' from your profile page. A reset link will be emailed to you.";
+}
+function show_ans4(){
+    document.getElementById("q4").innerHTML = "No, the subscription can not be cancelled in between, however it will be automatically expire after the given time period.";
+}
+function show_ans5(){
+    document.getElementById("q5").innerHTML = "Yes, additional support is provided by our service team.";
+}
